d1301f26be803c7d94fc76346725c90687fe1f62
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-05 01:56:57 +0100

    task 1 checking

--- shell.c
+++ shell.c
@@ -12,14 +12,13 @@
 
 int main(void)
 {
-	char *line = NULL;
-	char cwd[MAX_LENGTH];
+	char *line = NULL, *token, char cwd[MAX_LENGTH];
 	int status;
 	size_t line_len = 0;
 	ssize_t read_line;
 
 	pid_t process;
-	char *command[MAX_LENGTH] = {NULL, NULL};
+	char *const args[MAX_LENGTH] = {NULL, NULL};
 
 	while (1)
 	{
@@ -33,32 +32,44 @@ int main(void)
 
 		if ((read_line = getline(&line, &line_len, stdin)) != -1)
 		{
-			if (line[strlen(line) - 1] == '\n')
-				line[strlen(line) - 1] = '\0';
-
-			command[0] = strtok(line, " ");
-
-			process = fork();
-			if (process < 0)
+			if (feof(stdin))
 			{
-				perror("Unable to create a new process");
+				printf("\n");
 				break;
 			}
-			else if (process == 0)
+			perror(command[0]);
+			break;
+		}
+
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = '\0';
+
+		token = strtok(line, " ");
+		args = {token, NULL};
+
+		process = fork();
+
+		if (process < 0)
+		{
+			perror("Unable to create a new process");
+			break;
+		}
+		else if (process == 0)
+		{
+			if (access(line, X_OK) == 0)
 			{
-				execve(command[0], command, NULL);
-				perror(command[0]);
-				exit(EXIT_FAILURE);
+				execve(token, command, NULL);
+				perror(token);
+				_exit(EXIT_FAILURE);
 			}
 			else
-				wait(&status);
-
-			if (read_line == -1)
 			{
-				perror(command[0]);
-				break;
+				perror(token);
+				_exit(EXIT_FAILURE);
 			}
 		}
+		else
+			wait(&status);
 	}
 
 	free(line);
