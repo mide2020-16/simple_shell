1686c8aae22d84e8b2a5f0dbaa4056f4b06b5253
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-05 01:27:53 +0100

    task 1 checking

--- shell.c
+++ shell.c
@@ -10,66 +10,57 @@
 
 #define MAX_LENGTH 1024
 
-int main(int ac, char **av)
+int main(void)
 {
-  char *line = NULL;
-  char cwd[MAX_LENGTH];
-  int status;
-  size_t line_len = 0;
-  ssize_t read_line;
+	char *line = NULL;
+	char cwd[MAX_LENGTH];
+	int status;
+	size_t line_len = 0;
+	ssize_t read_line;
 
-  pid_t process;
-  char *command[MAX_LENGTH] = {NULL, NULL};
+	pid_t process;
+	char *command[MAX_LENGTH] = {NULL, NULL};
 
-  if (ac != 1)
-  {
-    perror("Usage: simple_shell\n");
-    return (1);
-  }
+	while (1)
+	{
+		if (getcwd(cwd, sizeof(cwd)) == NULL)
+		{
+			perror("Failed to get the current working directory");
+			break;
+		}
 
-  while (1)
-  {
-    if (getcwd(cwd, sizeof(cwd)) == NULL)
-    {
-      perror("Failed to get the current working directory");
-      break;
-    }
-  
-    printf("$ ");
+		printf("$ ");
 
-    if ((read_line = getline(&line, &line_len, stdin)) != -1)
-    {
-      if (line[strlen(line) - 1] == '\n')
-        line[strlen(line) - 1] = '\0';
+		if ((read_line = getline(&line, &line_len, stdin)) != -1)
+		{
+			if (line[strlen(line) - 1] == '\n')
+				line[strlen(line) - 1] = '\0';
 
-      command[0] = line;
-      process = fork();
-      if (process < 0)
-      {
-        perror("Unable to create a new process");
-        exit(EXIT_FAILURE);
-        break;
-      }
-      else if (process == 0)
-      {
-        execve(command[0], (char *const *)command, NULL);
-        printf("%s: No such file or directory\n", command[0]);
-        break;
-      }
-      else
-      {
-        wait(&status);
-      }
+			command[0] = strtok(line, " ");
 
-      if (read_line == -1)
-      {
-        printf("%s: No such file or directory\n", av[1]);
-        break;
-        _exit(EXIT_FAILURE);
-      }
-    }
-  }
+			process = fork();
+			if (process < 0)
+			{
+				perror("Unable to create a new process");
+				break;
+			}
+			else if (process == 0)
+			{
+				execve(command[0], command, NULL);
+				perror(command[0]);
+				exit(EXIT_FAILURE);
+			}
+			else
+				wait(&status);
 
-  free(line);
-  return (0);
+			if (read_line == -1)
+			{
+				perror(command[0]);
+				break;
+			}
+		}
+	}
+
+	free(line);
+	return (0);
 }
