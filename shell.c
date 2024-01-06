c7c489a42fbcca7a216429233ecbf2588fdb38e5
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-06 11:31:17 +0100

    check task 2

--- shell.c
+++ shell.c
@@ -5,76 +5,153 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
-#include <fcntl.h>
-#include <dirent.h>
-
-/*#include "shell.h"*/
 
 #define MAX_LENGTH 1024
 
+volatile sig_atomic_t child_pid;
+
+/**
+ * sigint_handler - This helps to handle Ctrl+C
+ * @sigmun: this is a void variable
+*/
+
+void sigint_handler(int signum)
+{
+	(void)signum;
+
+	if (child_pid > 0)
+	{
+		kill(child_pid, SIGKILL);
+		printf("\nChild process terminated by Ctrl+C\n");
+	}
+
+	printf("$ ");
+	fflush(stdout);
+}
+
+/**
+ * print_error - Prints error message
+ * @operation: the operation
+ * @message: the message to pass
+ */
+
+void print_error(const char *operation, const char *message)
+{
+
+	while (1)
+	{
+		fprintf(stderr, "Error in %s: %s\n", operation, message);
+		break;
+	}
+}
+
+/**
+ * print_error_exit - Prints error message and exit
+ * @operation: the operation
+ * @message: The message to pass
+ */
+
+void print_error_exit(const char *operation, const char *message)
+{
+	print_error(operation, message);
+	exit(EXIT_FAILURE);
+}
+
+/**
+ * wait_signal - Signals during wait
+ * @status: the status of child process
+ */
+
+void wait_signal(int status)
+{
+	if (WIFEXITED(status))
+		printf("The child process exited with status %d\n", WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		printf("The child process terminated by signal %d\n", WTERMSIG(status));
+	else
+		printf("The child process did not terminate properly\n");
+}
+
+/**
+ * main - Entry point
+ * Return: 0 on success
+ */
+
 int main(void)
 {
-	char *line = NULL, *token, cwd[MAX_LENGTH];
-	int status;
-	size_t line_len = 0;
-	ssize_t read_line;
-
-	pid_t process;
-	char *args[MAX_LENGTH] = {NULL, NULL};
+	int i = 0, status, j;
+	pid_t pid;
+	char command[MAX_LENGTH], *token, *newargv[MAX_LENGTH], *exec_argv[MAX_LENGTH], cwd[MAX_LENGTH], *line;
+	size_t len = 0;
 
 	while (1)
 	{
 		if (getcwd(cwd, sizeof(cwd)) == NULL)
+			print_error("getcwd", "Failed to get current working directory");
+
+		printf("Simple Shell (%s)>: \n", cwd);
+
+		if (getline(&line, &len, stdin) == -1)
+			print_error("fgets", "Failed to read input");
+
+		if (line == NULL)
 		{
-			perror("Failed to get the current working directory");
+			break;
+		}
+		if (feof(stdin))
+		{
+			perror("feof");
 			break;
 		}
 
-		printf("$ ");
-
-		if ((read_line = getline(&line, &line_len, stdin)) != -1)
+		for (j = 0; j < strlen(line); j++)
 		{
-			if (feof(stdin))
-			{
-				printf("\n");
-				break;
-			}
+			command[j++] = *line;
+		}
 
-			perror(line);
+		command[j] = '\0';
+
+		if (command[strlen(command) - 1] == '\n')
+			command[strlen(command) - 1] = '\0';
+
+		for (token = strtok(command, " "); token != NULL; token = strtok(NULL, " "))
+		{
+			newargv[i++] = token;
+			if (i >= MAX_LENGTH - 1)
+				print_error("Tokenization", "Exceeded maximum number of arguments");
+		}
+
+		newargv[i] = NULL;
+		if (i == 0)
+			continue;
+
+		for (int k = 0; k < i; k++)
+			exec_argv[k] = newargv[k];
+
+		exec_argv[i] = NULL;
+		if (strcmp(newargv[0], "exit") == 0)
 			break;
 
-			if (line[strlen(line) - 1] == '\n')
-				line[strlen(line) - 1] = '\0';
-
-			token = strtok(line, " ");
-			args[0] = token;
-
-			process = fork();
-
-			if (process < 0)
-			{
-				perror("Unable to create a new process");
-				break;
-			}
-			else if (process == 0)
-			{
-				if (access(line, X_OK) == 0)
-				{
-					execve(token, (char *const *)args, NULL);
-					perror(token);
-				}
-				else
-				{
-					perror(token);
-					_exit(EXIT_FAILURE);
-				}
-			}
-
-			else
-				wait(&status);
+		pid = fork();
+		if (pid < 0)
+			print_error_exit("fork", "Failed to create a child process");
+		else if (pid == 0)
+		{
+			signal(SIGINT, SIG_DFL);
+			execve(newargv[0], (char *const *)exec_argv, NULL);
+			print_error_exit("execve", "Execution failed");
+		}
+		else
+		{
+			child_pid = pid;
+			signal(SIGINT, sigint_handler);
+			wait(&status);
+			wait_signal(status);
+			child_pid = 0;
 		}
 	}
 
 	free(line);
+
 	return (0);
 }
