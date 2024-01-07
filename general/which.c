5447065582e46e6ed3f429aaf1e5db8e5e31d712
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-07 17:46:18 +0100

    shell.c

--- general/which.c
+++ general/which.c
@@ -10,15 +10,10 @@
 
 int main(void)
 {
-	char cwd[MAX_LENGTH];
-	char *line;
+	char cwd[MAX_LENGTH], *line, command[MAX_LENGTH], *token, *new_argv[MAX_LENGTH];
 	size_t len;
 	ssize_t nread, j;
-	int i, k;
-	int status;
-	char command[MAX_LENGTH];
-	char *token;
-	char *new_argv[MAX_LENGTH];
+	int i, k, status;
 	pid_t create_process;
 
 	while (1)
@@ -28,6 +23,7 @@ int main(void)
 			perror("getcwd");
 			break;
 		}
+
 		line = NULL;
 		i = 0;
 
@@ -36,14 +32,10 @@ int main(void)
 		if ((nread = getline(&line, &len, stdin)) != -1)
 		{
 			for (j = 0; j < nread; j++)
-			{
-				command[j++] = line[j];
-			}
+				command[j] = line[j];
 
 			if (command[strlen(command) - 1] == '\n')
-			{
 				command[strlen(command) - 1] = '\0';
-			}
 		}
 
 		perror("getline");
@@ -62,11 +54,9 @@ int main(void)
 		new_argv[i] = NULL;
 
 		if (i == 0)
-		{
 			continue;
-		}
 
-		if (strcmp(new_argv, "exit") == 0)
+		if (strcmp(new_argv[0], "exit") == 0)
 		{
 			write(STDOUT_FILENO, "\n", 1);
 			exit(EXIT_FAILURE);
@@ -75,26 +65,19 @@ int main(void)
 		create_process = fork();
 
 		if (create_process < 0)
-		{
 			perror("fork");
-		}
 		else if (create_process == 0)
 		{
 			if (execve(new_argv[0], (char *const *)new_argv, NULL) == -1)
-			{
 				perror("execve");
-			}
-
 			exit(EXIT_FAILURE);
 		}
 		else
 		{
 			wait(&status);
 			
-			for (k = 0; new_argv[k] != NULL; k++)
-			{
+			for (k = 0; new_argv[k] != NULL; k++)_
 				free(new_argv[k]);
-			}
 		}
 	}
 
