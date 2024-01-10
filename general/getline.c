2d3141448b4e2e4ab32be8ee5a173c3dfcec7c1a
mide2020-16 <efuwapeayomide51@gmail.com>
2024-01-10 18:50:28 +0100

    checkers

--- general/getline.c
+++ general/getline.c
@@ -5,62 +5,7 @@
 
 #define INIT_BUF 1024
 
-/**
- * get_line - Gets a line from stream
- * @lineptr: where to put the content
- * @n: size of line
- * @stream: the file to target
- * Return: i on Success
-*/
-
-size_t get_line(char **lineptr, size_t *n, FILE *stream)
-{
-	int c;
-	size_t i = 0;
-	size_t new_size;
-	char *new_line;
-
-	if (*lineptr == NULL || n == NULL|| stream == NULL)
-		return (-1);
-
-	if (*lineptr == NULL || *n == 0)
-	{
-		*lineptr = (char *)malloc(INIT_BUF);
-		if (*lineptr == NULL)
-		{
-			return (-1);
-			exit(EXIT_FAILURE);
-		}
-		*n = INIT_BUF;
-	}
-
-	while ((c = fgetc(stream)) != EOF && c != '\n')
-	{
-		if (i == *n - 1)
-		{
-			new_size = *n * 2;
-			new_line = (char *)realloc(*lineptr, new_size);
-			if (new_line == NULL)
-			{
-				return (-1);
-				exit(EXIT_FAILURE);
-			}
-			free(*lineptr);
-			*lineptr = new_line;
-			*n = new_size;
-		}
-		(*lineptr)[i++] = (char)c;
-	}
-
-	if (c == EOF && i == 0)
-		return (-1);
-
-	(*lineptr)[i] = '\0';
-
-	return (i);
-}
-
-int main(void)
+/*int main(void)
 {
 	char *line = NULL;
 	size_t len = 0, unary = (size_t)-1;
@@ -115,3 +60,60 @@ int main(void)
 
 	return 0;
 }
+*/
+
+/**
+ * get_line - Gets a line from stream
+ * @lineptr: where to put the content
+ * @n: size of line
+ * @stream: the file to target
+ * Return: i on Success
+ */
+
+size_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	int c;
+	size_t i = 0;
+	size_t new_size;
+	char *new_line;
+
+	if (*lineptr == NULL || n == NULL|| stream == NULL)
+		return (-1);
+
+	if (*lineptr == NULL || *n == 0)
+	{
+		*lineptr = (char *)malloc(INIT_BUF);
+		if (*lineptr == NULL)
+		{
+			return (-1);
+			exit(EXIT_FAILURE);
+		}
+		*n = INIT_BUF;
+	}
+
+	while ((c = fgetc(stream)) != EOF && c != '\n')
+	{
+		if (i == *n - 1)
+		{
+			new_size = *n * 2;
+			new_line = (char *)realloc(*lineptr, new_size);
+			if (new_line == NULL)
+			{
+				return (-1);
+				exit(EXIT_FAILURE);
+			}
+			free(*lineptr);
+			*lineptr = new_line;
+			*n = new_size;
+		}
+		(*lineptr)[i++] = (char)c;
+	}
+
+	if (c == EOF && i == 0)
+		return (-1);
+
+	(*lineptr)[i] = '\0';
+
+	return (i);
+}
+
--- getline.c
+++ getline.c
@@ -4,12 +4,50 @@
 
 #define MAX_BUF 1024
 
+/*int main(int argc, char *argv[])
+{
+	char *line = NULL;
+	size_t len = 0;
+	size_t bytesRead;
+	FILE *file;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+		return 1;
+	}
+
+	file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return 1;
+	}
+
+	bytesRead = _getline(&line, &len, file);
+
+	if (bytesRead != (size_t)-1)
+	{
+		printf("Bytes read: %lu\n", bytesRead);
+		printf("Line: %s\n", line);
+	}
+	else
+	{
+		printf("Error reading line\n");
+	}
+
+	free(line);
+	fclose(file);
+	return 0;
+}*/
+
 /**
  * perr_return - Prints error and return
  * @message: the message to print
  * @num: return digit
  * Return: int
-*/
+ */
 int perr_return(char *message, int num)
 {
 	perror(message);
@@ -76,40 +114,4 @@ size_t _getline(char **lineptr, size_t *n, FILE *stream)
 	return (i);
 }
 
-int main(int argc, char *argv[])
-{
-	char *line = NULL;
-	size_t len = 0;
-	size_t bytesRead;
-	FILE *file;
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-		return 1;
-	}
-
-	file = fopen(argv[1], "r");
-
-	if (file == NULL)
-	{
-		perror("Error opening file");
-		return 1;
-	}
-
-	bytesRead = _getline(&line, &len, file);
-
-	if (bytesRead != (size_t)-1)
-	{
-		printf("Bytes read: %lu\n", bytesRead);
-		printf("Line: %s\n", line);
-	}
-	else
-	{
-		printf("Error reading line\n");
-	}
-
-	free(line);
-	fclose(file);
-	return 0;
-}
